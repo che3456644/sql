163e4b952c7dc1475a91a0211e2e15c9e05981ac
Chetan Bhagat <cbhagat191@gmail.com>
2024-09-23 19:21:16 +0530

    Add files via upload

---
+++ 5_SQL_Order_By_Clause.sql
@@ -0,0 +1,13 @@
+
+
+select * from [dbo].[Employees]
+order by salary 
+
+select * from [dbo].[Employees]
+order by salary desc
+
+select * from dbo.Employees
+order by FirstName asc, Salary desc
+
+select * from dbo.Employees
+order by Department asc, Salary desc
