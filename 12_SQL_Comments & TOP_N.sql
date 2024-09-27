01bc5ad39b502a5e6cdbd28514221a0ba3b19ebe
Chetan Bhagat <cbhagat191@gmail.com>
2024-09-27 20:22:46 +0530

    Add files via upload

---
+++ 12_SQL_Comments & TOP_N.sql
@@ -0,0 +1,27 @@
+
+
+-- hi i am learning ms sql server
+
+select * from dbo.Employees
+ 
+
+ /* 
+ hi 
+ i 
+ am
+ learning 
+ sql
+ server 
+ */
+
+ select * from dbo.EmployeeRecords
+
+ select * from Employees
+
+ select top 2 * from Employees
+
+ select top 5 * from Employees
+
+ select top 2 employeeid,salary from Employees
+
+ select top 4 firstname , lastname from Employees
