96183dcbad1991aed5ee5c48a8c0043c1d8fcf63
Chetan Bhagat <cbhagat191@gmail.com>
2024-10-03 22:59:05 +0530

    Add files via upload

---
+++ 22_SQL_Left & Right_Anti_Join_Example.sql
@@ -0,0 +1,15 @@
+
+
+select * from table1
+
+select * from table2
+
+-- left anti join
+select * from table1 left join table2
+on table1.C1 = table2.C1
+where table2.C1 is null 
+
+-- right anti join
+select * from table1 right join table2
+on table1.C1 = table2.C1
+where table1.C2 is null 
